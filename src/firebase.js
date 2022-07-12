58f204e4705f574c32f35dd57cc0404471a9dd0a
iamminho <minho100227@kakao.com>
2022-07-12 14:13:37 +0900

    Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3hPF0WdXwvNZCV-NDTW-r7aY962-D7d0",
+  authDomain: "neither-49a9b.firebaseapp.com",
+  projectId: "neither-49a9b",
+  storageBucket: "neither-49a9b.appspot.com",
+  messagingSenderId: "16900405955",
+  appId: "1:16900405955:web:edb9d83bbe1e9a02cd4076",
+};
+
+export default initializeApp(firebaseConfig);
